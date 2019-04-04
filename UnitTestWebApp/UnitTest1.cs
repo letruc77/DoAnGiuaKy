027dda82db2a7ce598203cc036b3b1147a2bd05d
letruc77 <44826071+letruc77@users.noreply.github.com>
2019-04-04 22:38:45 +0700

    Test hàm login

--- UnitTestWebApp/UnitTest1.cs
+++ UnitTestWebApp/UnitTest1.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using WebApplication8;
 namespace UnitTestWebApp
 {
     [TestClass]
     public class UnitTest1
     {
-        private WebApplication8 WebApp;
         [TestMethod]
         public void TestLogin()
         {
+            string UserName = "letruc9394@gmail.com";
+            string PassWord = " Sa@123456";
+            AccountController account = new AccountController();
+            LoginViewModel model = new LoginViewModel();
+            model.Email = UserName;
+            model.Password = PassWord;
+            model.RememberMe = true;
+            Assert(account.Login(model, returnUrl = null),true);
 
         }
     }
